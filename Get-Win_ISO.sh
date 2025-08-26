48829fb43e21b6084baaba312389895dd15382ae
Robert Rosiles Careers <robert.rosiles.careers@gmail.com>
2025-08-25 21:04:10 -0700

    Update Get-Win_ISO.sh

--- Get-Win_ISO.sh
+++ Get-Win_ISO.sh
@@ -11,14 +11,14 @@ DEST_DIR="$(whoami)/Downloads/ISOs"
 mkdir -p "$DEST_DIR"
 cd "$DEST_DIR" || exit
 
-# ISO Links (uncomment to Download ISO)
+# ISO Links (uncomment: { "https://*" # Windows *} to Download ISO)
 urls=(
-  "https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.271-1/virtio-win.iso" # Virtio-Win ISO
- # "https://go.microsoft.com/fwlink/?linkid=2293312&clcid=0x409&culture=en-us&country=us"  # Windows Server 2025
-  "https://go.microsoft.com/fwlink/p/?LinkID=2195280&clcid=0x409&culture=en-us&country=US" # Windows Server 2022
- # "https://go.microsoft.com/fwlink/p/?LinkID=2195167&clcid=0x409&culture=en-us&country=US" # Windows Server 2019
-  "https://go.microsoft.com/fwlink/?linkid=2289031&clcid=0x409&culture=en-us&country=us"  # Windows 11 Enterprise
- # "https://go.microsoft.com/fwlink/?linkid=2289029&clcid=0x409&culture=en-us&country=us"  # Windows 11 Enterprise LTSC
+ # "https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.271-1/virtio-win.iso" # Virtio-Win ISO
+ # "https://go.microsoft.com/fwlink/?linkid=2293312&clcid=0x409&culture=en-us&country=us"    # Windows Server 2025
+ # "https://go.microsoft.com/fwlink/p/?LinkID=2195280&clcid=0x409&culture=en-us&country=US"  # Windows Server 2022
+ # "https://go.microsoft.com/fwlink/p/?LinkID=2195167&clcid=0x409&culture=en-us&country=US"  # Windows Server 2019
+ # "https://go.microsoft.com/fwlink/?linkid=2289031&clcid=0x409&culture=en-us&country=us"    # Windows 11 Enterprise
+ # "https://go.microsoft.com/fwlink/?linkid=2289029&clcid=0x409&culture=en-us&country=us"    # Windows 11 Enterprise LTSC
 )
 
 # Download each ISO
